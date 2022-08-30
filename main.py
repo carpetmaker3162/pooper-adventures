5e171f7755bb68ca2ae48b993828fb29bf141226
can pooper <domesticatedhuman9999@gmail.com>
2022-08-30 12:58:12 -0400

    trying to fix gravity

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
-    def render(self, screen):
+    def draw(self, screen):
         screen.blit(self.image, self.rect.center)
 
 class Player(Entity):
@@ -33,8 +33,16 @@ class Player(Entity):
         super().__init__("assets/Sports-Ball-Transparent.png", x, y, width, height)
         self.x_speed = 0
         self.y_speed = 0
+        self.max_x_speed = 1
+        self.gravity = 9.807
 
-        self.jump_power = 3
+        self.jump_power = 1
+        
+        self.width = width
+        self.height = height
+
+        print(self.rect.bottom)
+        print(self.rect.top)
     
     def move(self, x, y, collidables):
         dx = x
@@ -57,13 +65,13 @@ class Player(Entity):
     
     def on_ground(self, collidables):
         for entity in collidables:
-            if (entity.rect.top >= self.rect.bottom) and (entity.rect.left <= self.rect.right or entity.rect.right >= self.rect.left):
+            if (entity.rect.top <= (self.rect.bottom + self.height / 2)) and (entity.rect.left >= self.rect.right or entity.rect.right <= self.rect.left):
                 return True
         return False
     
     def hitting_ceiling(self, collidables):
         for entity in collidables:
-            if (entity.rect.bottom <= self.rect.top) and (entity.rect.left <= self.rect.right or entity.rect.right >= self.rect.left):
+            if (entity.rect.bottom >= self.rect.top) and (entity.rect.left >= self.rect.right or entity.rect.right <= self.rect.left):
                 return True
         return False
     
@@ -79,32 +87,48 @@ class Player(Entity):
         elif keys[pygame.K_UP] or keys[pygame.K_w]:
             self.y_speed = -self.jump_power * 10
         
+        if not self.on_ground(collidables) and self.y_speed < 20:
+            self.y_speed += self.gravity * 0.06
+        
         self.move(self.x_speed, self.y_speed, collidables)
 
 class Crate(Entity):
     def __init__(self, x, y):
         super().__init__("assets/crate.png", x, y)
+        print(self.rect.bottom)
+        print(self.rect.top)
 
 class Game:
     def __init__(self, fps) -> None:
         self.screen = pygame.display.set_mode((960, 640))
         pygame.display.set_caption("can pooper's adventures in Poopland")
         self.clock = pygame.time.Clock()
-        self.screen.fill((255, 255, 255))
         self.stopped = False
         self.framecap = fps
         self.player = Player(100, 100, 100, 100)
+
+        self.collidables = pygame.sprite.Group()
+        self.collidables.add(Crate(100, 400))
     
     def process_events(self):
         # process keyboard events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                self.stopped = True
     
     def loop(self):
-        pygame.display.flip()
         while not self.stopped:
             self.process_events()
+
+            pygame.event.pump()
+            
+            self.screen.fill((255, 255, 255))
+            self.player.update(self.collidables)
+            self.player.draw(self.screen)
+            self.collidables.draw(self.screen)
+            
+            pygame.display.flip()
+            
             self.clock.tick(self.framecap)
 
 if __name__ == "__main__":
